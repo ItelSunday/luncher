80241627a7ddd83b71673b3554486f9fd25c8a6d
ItelSunday <iteldomingotech@gmail.com>
2019-03-15 05:38:51 -0700

    merge branch, updated reducers

--- luncher/src/reducers/index.js
+++ luncher/src/reducers/index.js
@@ -2,17 +2,11 @@ import { adminReducer } from './adminReducers';
 import { combineReducers } from 'redux';
 import { registerReducer } from './registerReducers';
 import { loginReducer } from './LogInReducers';
-import { registerReducer } from './registerReducers';
 import { publicReducer } from './publicReducers';
 
 export default combineReducers({
     loginReducer,
     adminReducer,
-<<<<<<< HEAD
-    publicReducer,
-=======
     publicReducer,    
->>>>>>> 10f084b0cb38b937c15201c176b02ec292665d84
     registerReducer
 })
-
