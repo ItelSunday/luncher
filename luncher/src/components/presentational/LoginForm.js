d2b25e0c2eddea484ed5c17596d6732a562a5ed6
Julian Moreno <julmo611@gmail.com>
2019-03-14 21:38:19 -0700

    Update from Cori

--- luncher/src/components/presentational/LoginForm.js
+++ luncher/src/components/presentational/LoginForm.js
@@ -28,12 +28,10 @@ class LoginForm extends React.Component {
     return (
       <div className="login-form">
         <form onSubmit={e => this.handleLogin(e)}>
-        <div className="luncherlogo" >
-          <img src={luncherLogo} width="120" height="109" alt="" />
-        </div>
-        <h1> Admin Login </h1>
-          <label htmlFor="username"></label>
-          <input className="userlogo"
+          <img src={luncherLogo} width="60" height="60" alt="" />
+          <label htmlFor="username">Username</label>
+          <input
+            className="userlogo"
             required
             type="text"
             name="username"
@@ -41,8 +39,9 @@ class LoginForm extends React.Component {
             onChange={this.handleChange}
             placeholder="Username"
           />
-          <label htmlFor="password"></label>
-          <input className="passlogo"
+          <label htmlFor="password">Password</label>
+          <input
+            className="passlogo"
             required
             type="password"
             name="password"
